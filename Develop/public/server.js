1b19327d1890921c517e57b0581850d1d98c21cf
Alex Tran <Lockey5891@aol.com>
2024-06-14 11:32:26 -0700

    added codes to notes.js

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const path = require('path');
-
+const notes = require('notes')
 const app = express();
 const PORT = 3001;
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
