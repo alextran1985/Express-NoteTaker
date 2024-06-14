179451645c465620294f04699a51c117dd216e17
Alex Tran <Lockey5891@aol.com>
2024-06-14 10:52:44 -0700

    added fsUtils.js to helper folder

---
+++ Develop/public/notes.js
@@ -0,0 +1,35 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../db/helpers/fsUtils.js');
+
+// GET Route for retrieving all the saved notes
+notes.get('/', (req, res) =>
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting notes
+notes.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { notes } = req.body;
+  
+    // If all the required properties are present
+    if (notes) {
+      // Variable for the object we will save
+      const newNotes = {
+       notes,
+      };
+  
+      readAndAppend(newNotes, './db/notes.json');
+  
+      const response = {
+        status: 'success',
+        body: newNotes,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting notes');
+    }
+  });
+  
+  module.exports = notes;
